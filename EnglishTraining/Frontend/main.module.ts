a3573c8cbc0a180755e267790cc7462d7a93fe46
Yuri Partitski <PartySky@users.noreply.github.com>
2017-12-03 23:46:56 +0200

    first commit

---
+++ EnglishTraining/Frontend/main.module.ts
@@ -0,0 +1,8 @@
+import * as angular from "angular";
+
+import * as trackListComponentsModule from "./tracklist/tracklist.module";
+
+angular
+    .module('EnglishTraining.fullstack', [
+        trackListComponentsModule.name,
+    ]);
