e4a6c5971d0217428b26f5a9088136331fd72bdc
Yuri Partitski <PartySky@users.noreply.github.com>
2017-12-07 15:05:23 +0200

    Started to make a word model

--- EnglishTraining/Parser.cs
+++ EnglishTraining/Parser.cs
@@ -1,13 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.IO;
-using System.Net.Http;
-using Microsoft.VisualBasic;
 using System.Net;
 using Newtonsoft.Json;
-using System.Text;
+using WaveChart.models;
 
 namespace WaveChart
 {
@@ -15,9 +10,8 @@ namespace WaveChart
     {
         public void Download()
         {
-            LoadJson();
+            VmWord words = GetWords();
             var filepath = Path.Combine("./parserTests", "hello3.png");
-            var filepath2 = Path.Combine("./parserTests", "hello4.png");
 
             HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create("https://www.google.ru/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png");
             HttpWebResponse response = (HttpWebResponse)request.GetResponseAsync().Result;
@@ -26,38 +20,26 @@ namespace WaveChart
             {
                 responseStream.CopyTo(fileStream);
             }
-            using (FileStream fileStream = new FileStream(filepath2, FileMode.Create))
-            {
-                responseStream.CopyTo(fileStream);
-            }
-
-            //FileStream fileStream = new FileStream(filepath, FileMode.Create);
-            //responseStream.CopyTo(fileStream);
-            //fileStream.Dispose();
         }
         
-        public void LoadJson()
+        static VmWord GetWords()
         {
-            string jsonPath = "./parserTests/words.json";
+            VmWord words;
+            string jsonPath = "./jsons/words.json";
             if (!File.Exists(jsonPath))
             {
                 Console.WriteLine("File doesn't exist, path: {0}", jsonPath);
                 throw new ArgumentNullException(jsonPath);
             }
             // read file into a string and deserialize JSON to a type
-            Movie movie1 = JsonConvert.DeserializeObject<Movie>(File.ReadAllText(jsonPath));
+            VmWord movie1 = JsonConvert.DeserializeObject<VmWord>(File.ReadAllText(jsonPath));
             // deserialize JSON directly from a file
             using(StreamReader file = File.OpenText(jsonPath))
             {
                 JsonSerializer serializer = new JsonSerializer();
-                Movie movie2 = (Movie)serializer.Deserialize(file, typeof(Movie));
+                words = (VmWord)serializer.Deserialize(file, typeof(VmWord));
             }
-        }
-
-        public class Movie
-        {
-            public string Name { get; set; }
-            public int Year { get; set; }
+            return words;
         }
     }
 }
---
+++ EnglishTraining/models/VmWord.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WaveChart.models
+{
+    public class VmWord
+    {
+        public IList<string> items { get; set; }
+    }
+}
