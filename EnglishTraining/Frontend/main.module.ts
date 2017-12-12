adb20800e1c6ea62b54efabd6334a48aa787b495
Yuri Partitski <PartySky@users.noreply.github.com>
2017-12-12 09:40:44 +0200

    Some retro, went back to scope inheritance

--- EnglishTraining/Frontend/main.module.ts
+++ EnglishTraining/Frontend/main.module.ts
@@ -5,4 +5,23 @@ import * as TrackList from "./track-list/track-list.module";
 angular
     .module('EnglishTraining', [
         TrackList.name,
-    ]);
+    ])
+    // ;
+    // .controller('ExampleController', ['$scope', '$document', function ($scope, $document) {
+    //     $scope.title = $document[0].title;
+    //     $scope.windowTitle = angular.element(window.document)[0].title;
+    // }]);
+    .controller('ExampleController', ['$scope', '$document', function ($scope, $document) {
+        $scope.title = $document[0].title;
+        $scope.windowTitle = angular.element(window.document)[0].title;
+        $scope.check2 = function () {
+            console.log("check2()");
+            console.log();
+        }
+    }]);
+    // .controller('myCtrl', ['$scope', function ($scope) {
+    //     $scope.count = 0;
+    //     $scope.myFunc = function () {
+    //         $scope.count++;
+    //     };
+    // }]);
