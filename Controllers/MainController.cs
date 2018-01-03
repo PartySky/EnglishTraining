750830e093bfcac8fce12e9310e164e8a1da6fd7
Yuri Partitski <PartySky@users.noreply.github.com>
2018-01-03 23:35:06 +0200

    added main controller, added methods for getting and updating words

---
+++ Controllers/MainController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace EnglishTraining
+{
+    public class MainController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // TODO: use correct model
+        public async Task<VmCurrentWord> GetWords()
+        {
+            return await Task<VmCurrentWord>.Factory.StartNew(() =>
+            {
+                var testObject = new VmCurrentWord
+                {
+                    Name_ru = "кот",
+                    Name_en = "kat"
+                };
+                return testObject;
+            });
+        }
+
+        // TODO: use correct model
+        public async Task<VmCurrentWord> UpdateWords()
+        {
+            return await Task<VmCurrentWord>.Factory.StartNew(() =>
+            {
+                var testObject = new VmCurrentWord
+                {
+                    Name_ru = "кот",
+                    Name_en = "kat"
+                };
+                // TODO: return true and false
+                return testObject;
+            });
+        }
+    }
+}
