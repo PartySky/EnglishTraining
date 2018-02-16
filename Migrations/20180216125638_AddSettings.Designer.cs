d4203eaa60b22c82c1d5fbf3f9110156f610a4b5
PartySky <PartySky@users.noreply.github.com>
2018-02-16 14:58:43 +0200

    added migration

--- Migrations/20180216125638_AddSettings.Designer.cs
+++ Migrations/20180216125638_AddSettings.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace EnglishTraining.Migrations
 {
     [DbContext(typeof(WordContext))]
-    partial class WordContextModelSnapshot : ModelSnapshot
+    [Migration("20180216125638_AddSettings")]
+    partial class AddSettings
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20180216125638_AddSettings.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace EnglishTraining.Migrations
+{
+    public partial class AddSettings : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Settings",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    DailyTimeAmount = table.Column<int>(nullable: true),
+                    DailyWordsAmount = table.Column<int>(nullable: true),
+                    LearningLanguage = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Settings", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Settings");
+        }
+    }
+}
