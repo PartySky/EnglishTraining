35a0c7d29f57c824cd372fb14617ce1b4e690a59
Yuri Partitski <PartySky@users.noreply.github.com>
2018-03-11 16:37:24 +0200

    Added word finder

--- Program.cs
+++ Program.cs
@@ -1,15 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Net;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
 namespace EnglishTraining
 {
@@ -20,24 +10,8 @@ namespace EnglishTraining
 
             Parser parser = new Parser();
             NewCollocationParser collocationParser = new NewCollocationParser();
+            WordFinder wordFinder = new WordFinder();
             string runMode = "Host";
-
-            const string FILENAME = @"c:\temp\test.txt";
-
-            string input = File.ReadAllText(FILENAME);
-
-            string pattern = @"steam";
-            MatchCollection matches = Regex.Matches(input, pattern, RegexOptions.Singleline);
-            foreach (Match match in matches)
-            {
-                Console.WriteLine(match.Groups["steam"]);
-            }
-            Console.ReadLine();
-
-
-            StringReader reader = new StringReader(input);
-
-            string[] words = reader.ReadToEnd().Split(' ');
 
             switch (runMode)
             {
@@ -66,6 +40,9 @@ namespace EnglishTraining
                     parser.UpdateDictionary();
                     break;
 
+                case "Word finder":
+                    wordFinder.FindNewWords();
+                    break;
             }
         }
 
---
+++ WordFinder.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace EnglishTraining
+{
+    public class WordFinder
+    {
+        public string newWordsPath = Path.Combine(Directory.GetCurrentDirectory(), "txt", "new-words.txt");
+        public void FindNewWords()
+        {
+            VmWord[] wordsDB;
+            string excludedWordsFile = Path.Combine(Directory.GetCurrentDirectory(), "txt", "excluded-words.txt");
+
+            List<string> excludedWords = new HashSet<string>(excludedWordsFile
+                                                             .Split(new[] { '\r', '\n', ' ' })).ToList();
+
+            string newWordsFile = Path.Combine(Directory.GetCurrentDirectory(), "txt", "text-for-word-finding.txt");
+
+            string input = File.ReadAllText(newWordsFile);
+
+            using (var db = new WordContext())
+            {
+                wordsDB = db.Words.Where(p => (p.Name_ru.IndexOf(' ') < 0)
+                                         && (p.Name_en.IndexOf(' ') < 0)).ToArray();
+            }
+
+            List<string> words = new HashSet<string>(input.Split(new[] { '\r', '\n', ' ' })).ToList();
+            var newWords = words.Where(p => !wordsDB.Any(z => z.Name_en == p)
+                                         && !excludedWords.Any(z => z == p)).ToList();
+
+            using (StreamWriter file = File.CreateText(newWordsPath))
+            {
+                foreach(string word in newWords){
+                    file.WriteLine(word);
+                    Console.Write(word);
+                }
+            }
+        }
+    }
+}
