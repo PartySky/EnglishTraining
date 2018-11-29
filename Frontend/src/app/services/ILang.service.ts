afbd7b1c29ede2895abfd7b8af2f1185fb0f131e
Yuri Partitski <PartySky@users.noreply.github.com>
2018-11-29 04:53:49 +0200

    update

---
+++ Frontend/src/app/services/ILang.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export abstract class ILangService {
+    // tslint:disable-next-line:no-empty-interface
+    // export interface ILangService {
+
+}
